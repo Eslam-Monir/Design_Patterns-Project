2e9f315e88453c046de2d51322284c97b18fc203
Eslam-Monir <emoniream@gmail.com>
2023-12-14 22:57:04 +0200

    moved category functions from admin class to StoreSystem

--- src/StoreSystem.java
+++ src/StoreSystem.java
@@ -13,6 +13,40 @@ public class  StoreSystem {
         return instance;
     }
 
-    // Creates a Connection and returns a statement that could be used for DB statements
+    public  void  add_Category(String category,Connection conn){
+        try {
+            String sql = "INSERT INTO category (name) values('"+category +"')";
+            PreparedStatement statement = conn.prepareStatement(sql);
+            statement.executeUpdate();
+
+        }
+        catch(Exception e){
+            System.out.println(e.getMessage());
+        }
+
+    }
+
+    public  void edit_category(String old_name,String new_name,Connection conn){
+
+        try {
+            String sql = "UPDATE Category SET name='" + new_name + "'WHERE name='" + old_name + "'";
+            PreparedStatement statement = conn.prepareStatement(sql);
+            statement.executeUpdate();
+        }
+        catch (Exception e){
+            System.out.println(e.getMessage());
+        }
+    }
+    public  void delete_category(String name,Connection conn){
+
+        try {
+            String sql = "DELETE FROM Category WHERE name='"+name+"'";
+            PreparedStatement statement = conn.prepareStatement(sql);
+            statement.executeUpdate();
+        }
+        catch (Exception e){
+            System.out.println(e.getMessage());
+        }
+    }
 
 }
